e9ff123d8f3419b341e7e0845db70899d04deef5
DiegoR003 <168403198+DiegoR003@users.noreply.github.com>
2025-05-19 20:02:43 -0700

    otra mas

--- etercabo/src/App.jsx
+++ etercabo/src/App.jsx
@@ -16,6 +16,30 @@ function App() {
         <p>Humedad: 45%</p>
       </div>
     </div>
+
+    
+
+  );
+}
+
+
+ function dashboard() {
+  return (
+    <div className="container">
+      <h1>Sistema de Monitoreo de Calidad del Aire</h1>
+
+      <div className="tarjeta-sensor">
+        <img src={sensorImg} alt="Sensor" className="sensor-img" />
+        <h2>Dispositivo 1</h2>
+        <p>CO₂: 410 ppm</p>
+        <p>PM2.5: 12 µg/m³</p>
+        <p>Temperatura: 23°C</p>
+        <p>Humedad: 45%</p>
+      </div>
+    </div>
+
+    
+
   );
 }
 
